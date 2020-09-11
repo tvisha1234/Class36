1080bd60b89820726a1d5b2e9af264981db41526
tvisha1234 <65931904+tvisha1234@users.noreply.github.com>
2020-09-11 17:56:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+var allPlayers;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  background("black");
+
+  if(playerCount === 4) {
+    game.update(1);
+  }
+
+  if(gameState === 1) {
+    background("black");
+    clear();
+    game.play();
+  }
+}
